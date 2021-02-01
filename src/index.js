148384ef4fedd4c39b95031af7ae771ced3b44d3
mhnpd <mhnpd@outlook.com>
2021-02-01 14:22:52 +0545

    Updated the code implementation

--- src/index.js
+++ src/index.js
@@ -1,75 +1,14 @@
-import React, { useEffect, useState } from "react";
-import PropTypes from "prop-types";
-import { Spinner } from "./loader";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import "bootstrap/dist/css/bootstrap.min.css"
 
-const componentNames = [
-  "Audio",
-  "BallTriangle",
-  "Bars",
-  "Circles",
-  "Grid",
-  "Hearts",
-  "Oval",
-  "Puff",
-  "Rings",
-  "TailSpin",
-  "ThreeDots",
-  "Watch",
-  "RevolvingDot",
-  "Triangle",
-  "Plane",
-  "MutatingDots",
-  "CradleLoader"
-];
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-function componentName(type) {
-  if (componentNames.includes(type)) {
-    return Spinner[type];
-  }
-  return Spinner.Audio;
-}
 
-/**
- * @return {null}
- */
-export default function Loader(props) {
-  const [display, setDisplay] = useState(true);
-
-  useEffect(() => {
-    let timer;
-    if (props.timeout && props.timeout > 0) {
-      timer = setTimeout(() => {
-        setDisplay(false);
-      }, props.timeout);
-    }
-
-    return () => {
-      if (timer) clearTimeout(timer);
-    };
-  });
-
-  if (!props.visible || props.visible === "false") {
-    return null;
-  }
-  return display ? (
-    <div aria-busy="true" className={props.className} style={props.style}>
-      {React.createElement(componentName(props.type), { ...props })}
-    </div>
-  ) : null;
-}
-
-Loader.propTypes = {
-  type: PropTypes.oneOf([...componentNames]),
-  style: PropTypes.objectOf(PropTypes.string),
-  className: PropTypes.string,
-  visible: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
-  timeout: PropTypes.number
-};
-
-Loader.defaultProps = {
-  type: "Audio",
-  style: {},
-  className: "",
-  visible: true,
-  timeout: 0
-};
